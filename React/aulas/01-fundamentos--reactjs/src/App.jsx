3d0659efaac39c9ec7ea73dafb20dd02a6e698ea
Fernando <fsazevedo09@gmail.com>
2023-07-11 07:32:44 -0300

    Second Commit

---
+++ React/aulas/01-fundamentos--reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Post
+        autor="Fernando Azevedo"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus ad adipisci a saepe cumque aliquid odio suscipit libero quo odit totam autem animi quisquam cum, sed quidem explicabo rem aliquam."
+      />
+    </>
+  )
+}
---
+++ React/aulas/01-fundamentos--reactjs/src/Post.jsx
@@ -0,0 +1,4 @@
+
+export function Post(props) {
+    return <p>{props.content}</p>
+}
