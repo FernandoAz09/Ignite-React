567101300600f5e08ea31614a49cf44bce0d0358
Fernando <fsazevedo09@gmail.com>
2023-07-16 18:01:05 -0300

    Saindo da Programação imperativa para a declarativa com o REACT

--- React/aulas/01-fundamentos--reactjs/src/components/Comment.jsx
+++ React/aulas/01-fundamentos--reactjs/src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { Avatar } from './Avatar'
 
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({ content }) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/luizazevedo.png" />
@@ -18,7 +18,7 @@ export function Comment() {
                             <Trash size={24} />
                         </button>
                     </header>
-                    <p>Muito bom Fernando, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
--- React/aulas/01-fundamentos--reactjs/src/components/Post.jsx
+++ React/aulas/01-fundamentos--reactjs/src/components/Post.jsx
@@ -10,9 +10,10 @@ import styles from './Post.module.css'
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        1,
-        2,
+        "Muito bom Fernando, parabéns!! 👏👏"
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
@@ -25,9 +26,14 @@ export function Post({ author, publishedAt, content }) {
 
     function handleCreateNewComment() {
         event.preventDefault()
-        
-        setComments([...comments, comments.length + 1])
+
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
     }
+
+function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+}
 
     return (
         <article className={styles.post}>
@@ -60,11 +66,14 @@ export function Post({ author, publishedAt, content }) {
                 </p>
             </div>
 
-            <form  onSubmit={handleCreateNewComment} className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong> Deixe seu feedback</strong>
 
                 <textarea
+                    name="comment"
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -74,7 +83,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment} />
                 })}
             </div>
         </article>
