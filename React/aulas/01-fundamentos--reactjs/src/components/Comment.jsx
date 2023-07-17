373c6dcb6401461de4963a46ffb250fc550ab340
Fernando <fsazevedo09@gmail.com>
2023-07-17 07:03:24 -0300

    Delete funcionando + imutabilidade

--- React/aulas/01-fundamentos--reactjs/src/components/Comment.jsx
+++ React/aulas/01-fundamentos--reactjs/src/components/Comment.jsx
@@ -3,10 +3,10 @@ import { Avatar } from './Avatar'
 
 import styles from './Comment.module.css'
 
-export function Comment({ content, deleteComment }) {
+export function Comment({ content, onDeleteComment }) {
 
 function handleDeleteComment() {
-    deleteComment(content)
+    onDeleteComment(content)
 }
 
     return (
--- React/aulas/01-fundamentos--reactjs/src/components/Post.jsx
+++ React/aulas/01-fundamentos--reactjs/src/components/Post.jsx
@@ -35,8 +35,12 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value)
     }
 
-    function deleteComment(comment) {
-        console.log(`Deletar comentário ${comment}`)
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment =>{
+            return comment !== commentToDelete
+        })
+
+        setComments(commentsWithoutDeletedOne)
     }
 
     return (
@@ -91,7 +95,7 @@ export function Post({ author, publishedAt, content }) {
                         <Comment
                             key={comment}
                             content={comment}
-                            deleteComment={deleteComment}
+                            onDeleteComment={deleteComment}
                         />
                     )
                 })}
