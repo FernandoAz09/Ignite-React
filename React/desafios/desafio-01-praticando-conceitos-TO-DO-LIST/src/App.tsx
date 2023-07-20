57f621239198df326aeba02d80395d7a85720545
Fernando <fsazevedo09@gmail.com>
2023-07-20 07:41:43 -0300

    Adicionado form para adição de task + estilização

--- React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/App.tsx
+++ React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header"
+import { PlusCircle } from "@phosphor-icons/react"
 
 import './global.css'
 import styles from './App.module.css'
@@ -9,9 +10,10 @@ export function App() {
     <>
       <Header />
       <div className={styles.wrapper}>
-        <main>
-
-        </main>
+        <form className={styles.taskForm}>
+          <textarea placeholder="Adicione uma nova tarefa"></textarea>
+          <button type="submit">Criar <PlusCircle className={styles.plusIcon} size={20} /></button>
+        </form>
       </div>
     </>
   )
