072af3317f32a070b3c15c16f289cbc84d111774
Fernando <fsazevedo09@gmail.com>
2023-07-24 08:07:28 -0300

    Finalizada estilização da TASK

--- React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/components/TaskContainer.tsx
+++ React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/components/TaskContainer.tsx
@@ -1,18 +1,22 @@
 import styles from './Task.module.css'
-import { ClipboardText } from '@phosphor-icons/react'
+import { ClipboardText, Trash } from '@phosphor-icons/react'
 
 
-interface Task {
-    hasTask?: boolean
-}
-
-export function Task({ hasTask = false }) {
+export function Task() {
     return (
         <div className={styles.tasksWrapper}>
-            <div className={hasTask ? styles.task : styles.noTasks} >
+            {/* <div className={styles.noTasks} >
                 <div className={styles.taskIcon}><ClipboardText size={56} /></div>
                 <strong> Você ainda não tem tarefas cadastradas</strong>
                 <p>Crie tarefas e organize seus itens a fazer</p>
+            </div> */}
+
+            <div className={styles.hasTask} >
+                <div className={styles.task}>
+                    <input type="checkbox" />
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    <Trash size={20} />
+                </div>
             </div>
         </div>
     )
