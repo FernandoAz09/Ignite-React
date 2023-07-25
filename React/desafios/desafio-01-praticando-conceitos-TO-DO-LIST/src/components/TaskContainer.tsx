f3c8c61cd63e193d5749093deb5351278cf2f4e6
Fernando <fsazevedo09@gmail.com>
2023-07-25 07:26:18 -0300

    Update

--- React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/components/TaskContainer.tsx
+++ React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/components/TaskContainer.tsx
@@ -15,7 +15,7 @@ export function Task() {
                 <div className={styles.task}>
                     <input type="checkbox" />
                     <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                    <button title="Deletar tarefa">
+                    <button title="Deletar tarefa" className={styles.deleteButton}>
                         <Trash size={20} />
                     </button>
                 </div>
