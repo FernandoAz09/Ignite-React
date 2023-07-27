98c3a2b21eb7e30c85e036669d6e56912dbdda00
Fernando <fsazevedo09@gmail.com>
2023-07-27 07:41:07 -0300

    EmptyList criado

--- React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/components/TasksContainer.tsx
+++ React/desafios/desafio-01-praticando-conceitos-TO-DO-LIST/src/components/TasksContainer.tsx
@@ -27,7 +27,7 @@ export function TasksContainer({ tasks, onDelete, onComplete }: Props) {
                 </div>
             </header>
 
-            <div className={styles.list}>
+            <div className={tasks.length <= 0 ? styles.listEmpty : styles.list}>
                 {tasks.map((task) => (
                     <Task
                         key={task.id}
@@ -38,8 +38,16 @@ export function TasksContainer({ tasks, onDelete, onComplete }: Props) {
                 ))}
 
 
+                {tasks.length <= 0 && (
+                    <section className={styles.empty}>
+                        <ClipboardText size={50} />
+                        <div>
+                            <p>Você ainda não tem tarefas cadastradas</p>
+                            <span>Crie tarefas e organize seus itens a fazer</span>
+                        </div>
+                    </section>
+                )}
 
-                
             </div>
         </section>
 
