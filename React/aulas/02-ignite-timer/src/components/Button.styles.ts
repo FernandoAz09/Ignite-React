b8a25450d2ae2801b0c58e0350fc1bde32647ee9
Fernando <fsazevedo09@gmail.com>
2023-08-01 08:09:39 -0300

    Criado comando para identar e padronizar projeto

--- React/aulas/02-ignite-timer/src/components/Button.styles.ts
+++ React/aulas/02-ignite-timer/src/components/Button.styles.ts
@@ -3,31 +3,30 @@ import styled, { css } from 'styled-components'
 export type ButtonVariant = 'primary' | 'secundary' | 'success' | 'danger'
 
 interface ButtonContainerProps {
-    variant: ButtonVariant
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secundary: 'orange',
-    success: 'green',
-    danger: 'red'
+  primary: 'purple',
+  secundary: 'orange',
+  success: 'green',
+  danger: 'red',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    border-radius:4px;
-    border:none;
-    margin: 8px;
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: none;
+  margin: 8px;
 
-    background-color: ${props => props.theme['green-500']};
+  background-color: ${(props) => props.theme['green-500']};
 
-    color: ${props => props.theme.white};
+  color: ${(props) => props.theme.white};
 
-    /* ${props => {
-        return css`
-            background-color: ${buttonVariants[props.variant]};
-        `
-    }} */
-
+  /* ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }} */
 `
