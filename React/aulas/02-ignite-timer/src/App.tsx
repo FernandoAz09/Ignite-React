3d19d6ea5f152c4d575cb0c93bff9bf93925f57d
Fernando <fsazevedo09@gmail.com>
2023-08-01 07:28:50 -0300

    Iniciando projeto com styled-components

--- React/aulas/02-ignite-timer/src/App.tsx
+++ React/aulas/02-ignite-timer/src/App.tsx
@@ -1,6 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-
-  return <h1>Hello World!</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secundary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </ThemeProvider>
+  )
 }
 
---
+++ React/aulas/02-ignite-timer/src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+    primary: '#8256ef',
+    secundary: 'orange',
+    white: '#fff',
+}
