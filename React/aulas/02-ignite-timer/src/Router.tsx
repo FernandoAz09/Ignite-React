63ebd0b9b8abf697f1bac00d435ae73cc1bb826b
Fernando <fsazevedo09@gmail.com>
2023-08-03 18:42:01 -0300

    Estilizado DefaultLayout

--- React/aulas/02-ignite-timer/src/Router.tsx
+++ React/aulas/02-ignite-timer/src/Router.tsx
@@ -4,12 +4,12 @@ import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
-	return (
-		<Routes>
-			<Route path="/" element={<DefaultLayout />}>
-				<Route path="/" element={<Home />} />
-				<Route path="/history" element={<History />} />
-			</Route>
-		</Routes>
-	)
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
 }
