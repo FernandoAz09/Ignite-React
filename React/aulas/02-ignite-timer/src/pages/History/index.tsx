e6493a0b324e1a4567115d37f1866a5185d0cd0e
Fernando <fsazevedo09@gmail.com>
2023-08-08 08:28:23 -0300

    Criado componente History + estilização

--- React/aulas/02-ignite-timer/src/pages/History/index.tsx
+++ React/aulas/02-ignite-timer/src/pages/History/index.tsx
@@ -71,22 +71,6 @@ export function History() {
                 <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
           </tbody>
         </table>
       </HistoryList>
