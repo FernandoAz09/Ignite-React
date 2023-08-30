5060cfdd6e752d59184689ea52a46dd1eb5195bd
Fernando <fsazevedo09@gmail.com>
2023-08-30 08:10:36 -0300

    ajustado title

--- React/aulas/02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
+++ React/aulas/02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
@@ -57,6 +57,8 @@ export function Countdown() {
   useEffect(() => {
     if (activeCycle) {
       document.title = `${minutes}:${seconds}`
+    } else {
+      document.title = 'Ignite Timer'
     }
   }, [minutes, seconds, activeCycle])
 
