15f9318a33f8208bd0a9f1f7af2a73f66d8a3e24
Fernando <fsazevedo09@gmail.com>
2023-08-30 08:05:04 -0300

    History apresentando os dados. Iniciando Reducer para corrigir dados

--- React/aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ React/aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const newCylcleValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
     .number()
-    .min(1, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .min(1, 'O ciclo precisa ser de no mínimo 1 minutos.')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
