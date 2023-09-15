0ba4461c99282da297fd5ef9c9a312bac82537e2
Fernando <fsazevedo09@gmail.com>
2023-09-15 06:20:18 -0300

    Criado Layout

--- React/desafios/desafio-02-coffee-delivery/src/Routes.tsx
+++ React/desafios/desafio-02-coffee-delivery/src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Home";
 import { CompleteOrderPage } from "./pages/CompleteOrder";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/completeOrder" element={<CompleteOrderPage />} />
+            <Route path="/" element={<DefaultLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/completeOrder" element={<CompleteOrderPage />} />
+            </Route>
         </Routes>
     )
 }
